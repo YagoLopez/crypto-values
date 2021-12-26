06f9e7d431760aca8800e17d79aec33cf7d0a54d
Yago Lopez <yago.lopez@gmail.com>
2021-12-26 16:34:48 +0100

    Add styles to page3.tsx

--- apps/crypto-values/pages/page3.tsx
+++ apps/crypto-values/pages/page3.tsx
@@ -1,7 +1,6 @@
 // todo: sticky header and column for currencies names
 // todo: error control level api and component
 // todo: add tests
-// todo: remove styles
 
 import { CurrenciesMockRepository } from '../models/currency/repositories/CurrenciesMockRepository'
 import { useRepository } from '@crypto-values/react-query-crud'
@@ -12,37 +11,38 @@ import {
   getCurrencyChangesVector,
   getCurrencyNames,
 } from '../utils/functions'
-// import { MOCK_DATA } from '../utils/mock-data'
 import { FixedSizeGrid as Grid } from 'react-window'
+import CSS from './page3.module.css'
 
 export default function Page2() {
   const currenciesRepository = new CurrenciesMockRepository()
   const { useGetList } = useRepository(currenciesRepository)
   const { data: currenciesDataList, isLoading } = useGetList()
-  // const { data: currenciesDataList } = MOCK_DATA
 
   const currenciesChangesVector = getCurrencyChangesVector(currenciesDataList)
   const tableData = createChangesRatioMatrix(currenciesChangesVector)
   const currenciesNamesVector = getCurrencyNames(currenciesDataList)
-
-  // console.log(tableData)
 
   const Cell = ({ columnIndex, rowIndex, style }) => (
     <div style={style}>{tableData?.[rowIndex]?.[columnIndex]}</div>
   )
 
   return (
-    <div>
-      <Grid
-        columnCount={3497}
-        columnWidth={100}
-        height={500}
-        rowCount={3497}
-        rowHeight={35}
-        width={500}
-      >
-        {Cell}
-      </Grid>
+    <div className={CSS.content}>
+      {isLoading ? (
+        'Loading...'
+      ) : (
+        <Grid
+          columnCount={3497}
+          columnWidth={100}
+          height={500}
+          rowCount={3497}
+          rowHeight={35}
+          width={500}
+        >
+          {Cell}
+        </Grid>
+      )}
     </div>
   )
 }
