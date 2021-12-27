ff786d52094be9c68f8337de6322bb4036905ebc
Yago Lopez <yago.lopez@gmail.com>
2021-12-27 01:08:15 +0100

    Use real data in Page5

---
+++ apps/crypto-values/pages/page5/GridTable.tsx
@@ -0,0 +1,70 @@
+import { AutoSizer, MultiGrid } from 'react-virtualized'
+import styles from './gridtable.module.css'
+import { useState } from 'react'
+
+// const STYLE = {
+//   border: "1px solid #ddd"
+// };
+// const STYLE_BOTTOM_LEFT_GRID = {
+//   // borderRight: "2px solid #aaa",
+//   backgroundColor: "#f7f7f7"
+// };
+// const STYLE_TOP_LEFT_GRID = {
+//   borderBottom: "2px solid #aaa",
+//   // borderRight: "2px solid #aaa",
+//   fontWeight: "bold"
+// };
+// const STYLE_TOP_RIGHT_GRID = {
+//   borderBottom: "1px solid #aaa",
+//   fontWeight: "bold"
+// };
+
+export default ({ tableData }) => {
+  const [state] = useState({
+    fixedColumnCount: 1,
+    fixedRowCount: 1,
+    scrollToColumn: 0,
+    scrollToRow: 0,
+  })
+
+  const _cellRenderer = ({ columnIndex, key, rowIndex, style }) => {
+    return (
+      <div className={styles.Cell} key={key} style={style}>
+        {rowIndex === 0 ? (
+          <div>
+            Header
+            <br /> {`${columnIndex} - ${rowIndex}`}
+          </div>
+        ) : (
+          <span>{tableData?.[rowIndex]?.[columnIndex]}</span>
+        )}
+      </div>
+    )
+  }
+
+  return (
+    <AutoSizer disableHeight>
+      {({ width }) => (
+        <MultiGrid
+          {...state}
+          cellRenderer={_cellRenderer}
+          columnWidth={75}
+          columnCount={tableData.length}
+          enableFixedColumnScroll
+          enableFixedRowScroll
+          height={500}
+          rowHeight={70}
+          rowCount={tableData.length}
+          // style={STYLE}
+          // styleBottomLeftGrid={STYLE_BOTTOM_LEFT_GRID}
+          // styleTopLeftGrid={STYLE_TOP_LEFT_GRID}
+          // styleTopRightGrid={STYLE_TOP_RIGHT_GRID}
+          width={width}
+          hideTopRightGridScrollbar
+          hideBottomLeftGridScrollbar
+          hideBottomRightGridScrollbar
+        />
+      )}
+    </AutoSizer>
+  )
+}
---
+++ apps/crypto-values/pages/page5/index.tsx
@@ -0,0 +1,32 @@
+// todo: sticky header and column for currencies names
+// todo: error control level api and component
+// todo: add tests
+// todo: make HOC Loader
+// todo: add other char type
+
+import { CurrenciesMockRepository } from '../../models/currency/repositories/CurrenciesMockRepository'
+import { useRepository } from '@crypto-values/react-query-crud'
+import {
+  createChangesRatioMatrix,
+  createTableColumns,
+  getCurrencyChangesVector,
+  getCurrencyNames,
+} from '../../utils/functions'
+import CSS from './page4.module.css'
+import GridTable from './GridTable'
+
+export default function Page5() {
+  const currenciesRepository = new CurrenciesMockRepository()
+  const { useGetList } = useRepository(currenciesRepository)
+  const { data: currenciesDataList, isLoading } = useGetList()
+
+  const currenciesChangesVector = getCurrencyChangesVector(currenciesDataList)
+  const tableData = createChangesRatioMatrix(currenciesChangesVector)
+  const currenciesNamesVector = getCurrencyNames(currenciesDataList)
+
+  return (
+    <div className={CSS.App}>
+      {isLoading ? 'Loading...' : <GridTable tableData={tableData} />}
+    </div>
+  )
+}
