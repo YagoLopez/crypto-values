b5142c4d128fb8174207e4afb8ea3fa7208850ea
Yago Lopez <yago.lopez@gmail.com>
2021-12-29 00:40:26 +0100

    Refactor table styles

--- apps/crypto-values/pages/page5/GridTable.tsx
+++ apps/crypto-values/pages/page5/GridTable.tsx
@@ -1,102 +1,92 @@
+// todo: add tool tip to first row and column with "change value" text
 import { AutoSizer, MultiGrid } from 'react-virtualized'
 import styles from './gridtable.module.css'
 import { useState } from 'react'
 
-const STYLE = {
-  border: '1px solid #ddd',
+const TABLE_STYLE = {
+  border: '5px solid #ddd',
 }
-const STYLE_BOTTOM_LEFT_GRID = {
-  // borderRight: "2px solid #aaa",
+const STYLE_FIRST_CELL = {
+  background: '#f7f7f7',
+  borderBottom: '2px solid #aaa',
+}
+const STYLE_FIRST_ROW = {
+  background: '#f7f7f7',
+  borderBottom: '1px solid #aaa',
+}
+const STYLE_FIRST_COLUMN = {
   backgroundColor: '#f7f7f7',
 }
-const STYLE_TOP_LEFT_GRID = {
-  borderBottom: '2px solid #aaa',
-  // borderRight: "2px solid #aaa",
-  fontWeight: 'bold',
-}
-const STYLE_TOP_RIGHT_GRID = {
-  borderBottom: '1px solid #aaa',
-  fontWeight: 'bold',
+
+const STYLE_FIRST_ROW_CONTENT = {
+  height: 'inherit',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
 }
 
 export default function Page5({ tableData }) {
-  const [state] = useState({
+  const dimension = tableData?.length
+  const [state, setStateValues] = useState({
     fixedColumnCount: 1,
     fixedRowCount: 1,
     scrollToColumn: 0,
     scrollToRow: 0,
   })
 
-  // const _cellRenderer = ({ columnIndex, key, rowIndex, style }) => {
-  //   return (
-  //     <div className={styles.cell} key={key} style={style}>
-  //       {tableData[rowIndex][columnIndex]}
-  //     </div>
-  //   )
-  // }
-
-  const _cellRenderer = ({ columnIndex, key, rowIndex, style }) => {
-    return (
-      <div className={styles.cell} key={key} style={style}>
-        {rowIndex === 0 && (
-          <div>
-            {tableData[rowIndex][columnIndex].s}
-            <br />
-            {tableData[rowIndex][columnIndex].ch}
-          </div>
-        )}
-        {columnIndex === 0 && (
-          <div>
-            {tableData[rowIndex][columnIndex].s}
-            <br />
-            {tableData[rowIndex][columnIndex].ch}
-          </div>
-        )}
-        {rowIndex !== 0 && columnIndex !== 0 && (
-          <span>{tableData[rowIndex][columnIndex]}</span>
-        )}
-      </div>
-    )
+  const onResetScroll = () => {
   }
-  // const _cellRenderer = ({ columnIndex, key, rowIndex, style }) => {
-  //   return (
-  //     <div className={styles.cell} key={key} style={style}>
-  //       {rowIndex === 0 ? (
-  //         <div>
-  //           Header
-  //           <br />
-  //           {`${columnIndex} - ${rowIndex}`}
-  //         </div>
-  //       ) : (
-  //         <span>{tableData[rowIndex][columnIndex]}</span>
-  //       )}
-  //     </div>
-  //   )
-  // }
+
+  const _cellRenderer = ({ columnIndex, key, rowIndex, style }) => (
+    <div className={styles.cell} key={key} style={style}>
+      {rowIndex === 0 && (
+        <div data-id="first-row" style={STYLE_FIRST_ROW_CONTENT}>
+          {tableData[rowIndex][columnIndex].s}
+          <br />
+          {tableData[rowIndex][columnIndex].ch}
+        </div>
+      )}
+      {columnIndex === 0 && (
+        <div data-id="first-column" style={STYLE_FIRST_ROW_CONTENT}>
+          {tableData[rowIndex][columnIndex].s}
+          <br />
+          {tableData[rowIndex][columnIndex].ch}
+        </div>
+      )}
+      {rowIndex !== 0 && columnIndex !== 0 && (
+        <span data-id="submatrix" style={STYLE_FIRST_ROW_CONTENT}>
+          {tableData[rowIndex][columnIndex]}
+        </span>
+      )}
+    </div>
+  )
 
   return (
-    <AutoSizer disableHeight>
-      {({ width }) => (
-        <MultiGrid
-          {...state}
-          cellRenderer={_cellRenderer}
-          columnWidth={75}
-          columnCount={tableData?.length}
-          enableFixedColumnScroll
-          enableFixedRowScroll
-          height={500}
-          rowHeight={70}
-          rowCount={tableData?.length}
-          style={STYLE}
-          styleBottomLeftGrid={STYLE_BOTTOM_LEFT_GRID}
-          styleTopLeftGrid={STYLE_TOP_LEFT_GRID}
-          styleTopRightGrid={STYLE_TOP_RIGHT_GRID}
-          width={width}
-          hideTopRightGridScrollbar
-          hideBottomLeftGridScrollbar
-          hideBottomRightGridScrollbar
-        />
-      )}
-    </AutoSizer>
+    <>
+      <AutoSizer disableHeight>
+        {({ width }) => (
+          <MultiGrid
+            {...state}
+            cellRenderer={_cellRenderer}
+            columnWidth={75}
+            columnCount={dimension}
+            rowHeight={50}
+            rowCount={dimension}
+            height={500}
+            width={width}
+            enableFixedColumnScroll
+            enableFixedRowScroll
+            hideTopRightGridScrollbar
+            hideBottomLeftGridScrollbar
+            hideBottomRightGridScrollbar
+            style={TABLE_STYLE}
+            styleTopLeftGrid={STYLE_FIRST_CELL}
+            styleTopRightGrid={STYLE_FIRST_ROW}
+            styleBottomLeftGrid={STYLE_FIRST_COLUMN}
+          />
+        )}
+      </AutoSizer>
+      <button onClick={onResetScroll}>Reset Scroll Bars</button>
+    </>
   )
 }
