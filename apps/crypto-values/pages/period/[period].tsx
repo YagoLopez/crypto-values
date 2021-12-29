9a7e6491423948f9a1c740fd952f8fe7f050bc1b
Yago Lopez <yago.lopez@gmail.com>
2021-12-29 22:11:22 +0100

    Add dynamic route for period parameter

--- apps/crypto-values/pages/period/[period].tsx
+++ apps/crypto-values/pages/period/[period].tsx
@@ -1,34 +1,36 @@
 // todo: add tests
 // todo: make HOC Loader
-// todo: add other char type
 // todo: try branch with nextjs/pwa
 // todo: storybook?
 // todo: remove query-string npm package
-// todo: error boundaries
 // todo: add favico
+// todo: add number of rowns and columns as info
+// todo: preconnect to coin360.com domain
 
 import { CurrenciesMockRepository } from '../../models/currency/repositories/CurrenciesMockRepository'
 import { useRepository } from '@crypto-values/react-query-crud'
 import { createRatiosMatrix3 } from '../../utils/functions'
-import styles from './page5.module.css'
-import GridTable from './GridTable'
 import { useRouter } from 'next/router'
+import GridTable from './GridTable'
+import styles from './page5.module.css'
 
 export default function Page5() {
   const router = useRouter()
+  const { period } = router.query
   const currenciesRepository = new CurrenciesMockRepository()
   const { useGetList2 } = useRepository(currenciesRepository)
-  const { data: currenciesDataList, isLoading, error } = useGetList2(24)
+  const { data: currenciesDataList, isLoading, error } = useGetList2(+period)
   const tableData = createRatiosMatrix3(currenciesDataList)
 
   const onChangePeriod = (period: number): void => {
-    router.push(`/page5/period/${period}`, undefined, { shallow: true })
+    router.push(`/period/${period}`, undefined, { shallow: true })
   }
   if (isLoading) return 'Loading...'
   if (error) return 'An error has occurred: ' + (error as Error).message
 
   return (
     <div className={styles.App}>
+      <div>Dynamic route</div>
       <button onClick={() => onChangePeriod(1)}>1 day</button>
       <button onClick={() => onChangePeriod(24)}>24 days</button>
       <button onClick={() => onChangePeriod(48)}>48 days</button>
