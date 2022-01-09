dc3818378652e41521700e77e9c2302bd48451a1
Yago Lopez <yago.lopez@gmail.com>
2022-01-09 21:55:03 +0100

    Add animation Loader.tsx

--- apps/crypto-values/components/Loader/Loader.tsx
+++ apps/crypto-values/components/Loader/Loader.tsx
@@ -4,9 +4,7 @@ import AccessTimeIcon from '@mui/icons-material/AccessTime'
 export default function Loader({ text = 'Loading...' }) {
   return (
     <div className={styles.container}>
-      <div style={{ marginRight: '5px', marginTop: '5px' }}>
-        <AccessTimeIcon />
-      </div>
+      <AccessTimeIcon className={styles.rotateCenter} />
       {`${text}`}
     </div>
   )
